18aa4c5a7c7bd93616c44e7d61d7d00578a55f17
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-12-20 15:47:24 +0100

    Add comments to match descriptors

--- exercise_03/src/match_descriptors.py
+++ exercise_03/src/match_descriptors.py
@@ -9,20 +9,28 @@ def matchDescriptors(query_descriptors, database_descriptors, match_lambda):
     amount of query and database descriptors respectively. matches(i) will be -1 if there is no database descriptor
     with an SSD < lambda * min(SSD). No elements of matches will be equal except for the -1 elements.
     """
-    # print(query_descriptors)
-    # print(database_descriptors)
-    # print((query_descriptors - database_descriptors))
-
+    # shape: (Q, D) -- in this case (200, 200)
+    # distance from each query descriptor to each database descriptor
     dists = cdist(query_descriptors.T, database_descriptors.T, "euclidean")
-    print(dists)
-    print(dists.shape)
+
+    # shape: (200, 1)
+    # for each query_descriptor, which db_descriptor (index) is closest (argmin)
     matches = np.argmin(dists, axis=1)
+
+    # shape: (200, 1)
+    # keep only distances that matched in `matches`
     dists = dists[np.arange(matches.shape[0]), matches]
+
+    # scalar
+    # min distance between any two descriptors across both sets
     min_non_zero_dist = dists.min()
 
+    # keep only descriptors with small distance
+    # adaptive threshold (because there should be at least one match)
     matches[dists >= match_lambda * min_non_zero_dist] = -1
 
-    # remove double matches
+    # remove double matches:
+    # if a db_descriptor was assigned to several query_descriptors, keep only 1 match
     unique_matches = np.ones_like(matches) * -1
     _, unique_match_idxs = np.unique(matches, return_index=True)
     unique_matches[unique_match_idxs] = matches[unique_match_idxs]

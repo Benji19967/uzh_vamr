63af338b9e65aa303281abe17cbb6828726103d7
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-12-26 20:13:54 +0100

    Add image reading utils

--- sandbox/src/main.py
+++ sandbox/src/main.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
 from src import utils
+
+KITTI_DIR = Path("data/kitti")
+KITTI_IMAGES_DIR = KITTI_DIR / "05" / "image_0"
 
 
 def main() -> None:
 
-    utils.read_img()
+    img = utils.read_img(filepath=KITTI_IMAGES_DIR / "000000.png")
+    utils.show_img(img)
 
 
 if __name__ == "__main__":
--- sandbox/src/utils.py
+++ sandbox/src/utils.py
@@ -2,7 +2,24 @@ from pathlib import Path
 
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def read_img(filepath: Path) -> np.ndarray:
     return cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)  # type: ignore
+
+
+def show_img(img: np.ndarray, title: str | None = None):
+    plt.imshow(img, interpolation="nearest")
+    plt.gray()
+    if title:
+        plt.title(title)
+    plt.show()
+
+
+def show_img_cv(img: np.ndarray):
+    cv2.imshow("image", img)
+    cv2.waitKey(0)
+
+    # closing all open windows
+    cv2.destroyAllWindows()

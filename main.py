63ceac831a6eada942f075045879d15220e0ec05
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-08-02 10:37:25 +0200

    Add utils for resizing images

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from utils import image
+from utils.image import Image
 
 
 def main():
-    image.load()
+    image = Image(filepath="./data/iPhone7/IMG_5713.JPG")
+    print(image.shape())
+    image.show_cv()
+
+    print(image.resize(320, 480).shape)
+
+    print(image.resize_inplace(320, 480))
+    print(image.shape())
+    image.show_cv()
 
 
 if __name__ == "__main__":
--- utils/image.py
+++ utils/image.py
@@ -4,6 +4,10 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+"""
+Notes: cv2 works in (width, height) vs numpy in (height, width)
+"""
+
 
 def read_img(filepath: Path | str) -> np.ndarray:
     return cv2.imread(str(filepath), cv2.IMREAD_GRAYSCALE)  # type: ignore
@@ -19,20 +23,32 @@ def show_img(img: np.ndarray, title: str | None = None):
 
 def show_img_cv(img: np.ndarray):
     cv2.imshow("image", img)
+    cv2.setWindowProperty(
+        "image", cv2.WND_PROP_TOPMOST, 1
+    )  # show image as topmost window
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
 class Image:
-    def __init__(self) -> None:
-        self.img: np.ndarray
+    def __init__(self, filepath: str) -> None:
+        self.img: np.ndarray = read_img(filepath=filepath)
         self.filepath: str
-
-    def load(self, filepath: str) -> np.ndarray:
-        return read_img(filepath=filepath)
 
     def show(self, title: str | None = None) -> None:
         show_img(img=self.img, title=title)
 
     def show_cv(self) -> None:
         show_img_cv(img=self.img)
+
+    def shape(self) -> tuple[int, ...]:
+        """
+        (height, width)
+        """
+        return self.img.shape
+
+    def resize(self, width: int, height: int) -> np.ndarray:
+        return cv2.resize(self.img, (width, height))
+
+    def resize_inplace(self, width: int, height: int) -> None:
+        self.img = cv2.resize(self.img, (width, height))
